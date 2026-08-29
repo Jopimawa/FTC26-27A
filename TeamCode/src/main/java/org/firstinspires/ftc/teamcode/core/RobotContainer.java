25c839416456d428c5e93d59a053efeb65cb836c
jas <jas@box>
2026-08-29 11:36:59 -0400

    formatted bindings, renamed to gamepad for consistency

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/RobotContainer.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.core;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.seattlesolvers.solverslib.command.Command;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
@@ -16,22 +15,27 @@ import org.firstinspires.ftc.teamcode.subsystem.GameSubsystem;
 public class RobotContainer {
     private final DriveSubsystem drive;
     private final GameSubsystem game;
-    private final GamepadEx[] controller;
+    private final GamepadEx[] gamepad;
     private Telemetry telemetry;
-    public RobotContainer(HardwareMap hardwareMap, Telemetry telemetry, Gamepad controller1, Gamepad controller2) {
+    public RobotContainer(HardwareMap hardwareMap, Telemetry telemetry, Gamepad gamepad1, Gamepad gamepad2) {
         drive = new DriveSubsystem(hardwareMap);
         game = new GameSubsystem(hardwareMap);
-        controller = new GamepadEx[]{new GamepadEx(controller1), new GamepadEx(controller2)};
+        gamepad = new GamepadEx[]{new GamepadEx(gamepad1), new GamepadEx(gamepad2)};
         configureBindings();
     }
     public void configureBindings() {
-        drive.setDefaultCommand(new MoveWheel(drive, controller[0]::getLeftX, controller[0]::getLeftY, controller[0]::getRightX));
+        drive.setDefaultCommand(new MoveWheel(drive, gamepad[0]::getLeftX, gamepad[0]::getLeftY, gamepad[0]::getRightX));
 
-        controller[0].getGamepadButton(GamepadKeys.Button.A).whenHeld(new MoveIntake(game,1));
+        gamepad[0].getGamepadButton(GamepadKeys.Button.A)
+                  .whenHeld(new MoveIntake(game,1));
 
-        controller[0].getGamepadButton(GamepadKeys.Button.DPAD_UP).whenHeld(new MoveWheelRaw(drive,0.5,0,0,0));
-        controller[0].getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whenHeld(new MoveWheelRaw(drive,0,0.5,0,0));
-        controller[0].getGamepadButton(GamepadKeys.Button.DPAD_RIGHT).whenHeld(new MoveWheelRaw(drive,0.5,0,0.5,0));
-        controller[0].getGamepadButton(GamepadKeys.Button.DPAD_LEFT).whenHeld(new MoveWheelRaw(drive,0.5,0,0,0.5));
+        gamepad[0].getGamepadButton(GamepadKeys.Button.DPAD_UP)
+                  .whenHeld(new MoveWheelRaw(drive,0.5,0,0,0));
+        gamepad[0].getGamepadButton(GamepadKeys.Button.DPAD_DOWN)
+                  .whenHeld(new MoveWheelRaw(drive,0,0.5,0,0));
+        gamepad[0].getGamepadButton(GamepadKeys.Button.DPAD_RIGHT)
+                  .whenHeld(new MoveWheelRaw(drive,0.5,0,0.5,0));
+        gamepad[0].getGamepadButton(GamepadKeys.Button.DPAD_LEFT)
+                  .whenHeld(new MoveWheelRaw(drive,0.5,0,0,0.5));
     }
 }
